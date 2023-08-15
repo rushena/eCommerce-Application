8b90d3ab47d08949aad8ee9929eaf4553f7f9cba
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-08-15 22:37:52 +0300

    feat: implement city validation

---
+++ src/View/components/CityInput.ts
@@ -0,0 +1,46 @@
+// City: Must contain at least one character and no special characters or numbers
+
+
+import { checkIfContainsCharacters, checkPatternMatch } from "./Helpers";
+
+function showError(city:HTMLInputElement, cityError:HTMLSpanElement) {
+  if(checkIfContainsCharacters(city, cityError)){
+    checkPatternMatch(city, cityError, 'no special characters or numbers');
+  }
+ }
+
+function checkCityValidity(city: HTMLInputElement, cityError: HTMLSpanElement){
+
+  city.addEventListener('blur', () => {
+    console.log(city, cityError)
+    if (city.validity.valid) {
+      cityError.textContent = '';
+    } else {
+      showError(city, cityError);
+    }
+  });
+}
+
+export default function setCityValidity() {
+
+  const form = document.querySelector('.form') as HTMLFormElement;
+  const billingCity = document.querySelector('.billing-city') as HTMLInputElement;
+  const shippingCity = document.querySelector('.shipping-city') as HTMLInputElement;
+  
+  const billingCityError = document.querySelector('.billing-city ~ span.validation-message') as HTMLSpanElement;
+  const shippingCityError = document.querySelector('.shipping-city ~ span.validation-message') as HTMLSpanElement;
+
+  checkCityValidity(billingCity, billingCityError);
+  checkCityValidity(shippingCity, shippingCityError);
+
+  form.addEventListener('submit', (event) => {
+    if (!billingCity.validity.valid){
+      showError(billingCity, billingCityError);
+      event.preventDefault();
+    }
+    if (!shippingCity.validity.valid) {
+      showError(shippingCity, shippingCityError);
+      event.preventDefault();
+    }
+  });
+}
