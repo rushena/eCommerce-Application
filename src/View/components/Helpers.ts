c4717dfaa5cc343b18f73282b89d39058f6344aa
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-08-15 23:30:49 +0300

    feat: implement Postal code validation

--- src/View/components/Helpers.ts
+++ src/View/components/Helpers.ts
@@ -5,10 +5,13 @@ export function checkIfContainsCharacters(inputField: HTMLInputElement, errorFie
   } else return true;
 }
 
-export function checkPatternMatch(inputField: HTMLInputElement, errorField: HTMLSpanElement, patternType: 'no special characters or numbers'){
+export function checkPatternMatch(inputField: HTMLInputElement, errorField: HTMLSpanElement, patternType: "no special characters or numbers" | "Postal code does not correspond to country's format"){
   if (inputField.validity.patternMismatch) {
     if(patternType === 'no special characters or numbers'){
       errorField.textContent = "No special characters or numbers are allowed";
+    } 
+    if(patternType === "Postal code does not correspond to country's format"){
+      errorField.textContent = "Postal code does not correspond to country's format";
     } 
   }
 }
