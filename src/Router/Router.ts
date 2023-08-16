8b9e66fa0a8adca0b2e9252bcc05ceb1d828f7ed
Rushen Adjigeldieva <rushena2706@gmail.com>
2023-08-16 11:18:47 +0500

    feat: add simple structure for routing

--- src/Router/Router.ts
+++ src/Router/Router.ts
@@ -1,3 +1,29 @@
-export class Router {
+export function Router() {
+    this.routes = {
+        '/': {
+            title: 'Main',
+            renderFn: alert
+        },
+        '/user/authorization': {
+            title: 'Authorization',
+            renderFn: alert
+        },
+        '/user/registration': {
+            title: 'Registration',
+            renderFn: alert
+        },
+    };
 
+    this.get = function(url: string) {
+        console.log(url);
+    }
+
+    this.addRoute = function(path, title, cb) {
+        this.routes[path] = {
+            title,
+            renderFn: cb
+        };
+
+        return this;
+    }
 }
