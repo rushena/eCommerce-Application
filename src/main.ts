2117f2b9b2d3b55ec0dd95796de8ea1097aab5b7
Nikita Mikheitsev <welt992@gmail.com>
2023-08-17 11:40:24 +0200

    fix: minor styles changes

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import Header from './View/pages/header.ts';
 import { footer } from './View/pages/footer.ts';
 
 const check = localStorage.getItem('check') === 'true';
-let option;
+let option: { isLogged: boolean; cartItems: number };
 if (check === null) {
   option = { isLogged: true, cartItems: 0 };
 } else {
   option = { isLogged: check, cartItems: 0 };
 }
 const header = Header.getInstance(option);
+setTimeout(() => {
+  const header1 = Header.getInstance(option);
+  header1.cartElement = 10;
+}, 4000);
 header.cartElement = 0;
 footer();
