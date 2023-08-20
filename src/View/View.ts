9a9c20fb21e9368b717657443bcedeb02577e67e
Rushen Adjigeldieva <rushena2706@gmail.com>
2023-08-20 21:43:06 +0500

    feat: add render functions in View.ts

--- src/View/View.ts
+++ src/View/View.ts
@@ -1,4 +1,10 @@
 import {Routing} from "../Router/Router";
+import {createFooter} from './components/footer';
+import {Header} from './components/header';
+import {PageNotFound} from './pages/404.page';
+import {MainPage} from "./pages/Main.page";
+import createLoginPage from "./pages/login";
+import createRegistrationPage from "./pages/registration";
 
 interface IView {
   routing: Routing,
@@ -10,44 +16,34 @@ export class View implements IView {
 
   static readonly $mainContent: HTMLElement = document.createElement('div');
   private routing!;
+  private readonly $footer = createFooter();
+  private readonly $header = new Header().element;
+  private readonly $notFound = new PageNotFound().getPageCode();
+  private readonly $mainPage = new MainPage().getElement();
+  private readonly $loginPage = createLoginPage();
+  private readonly $RegistrationPage = createRegistrationPage();
 
   setRouting(routing: Routing) {
     this.routing = routing;
   }
 
   renderStartElements(): void {
-    const $header = document.createElement('div');
-    $header.innerHTML = `
-      <ul>
-        <li><a href="/">Main</a></li>
-        <li><a href="/user/authorization">authorization</a></li>
-        <li><a href="/user/registration">registration</a></li>
-        <li><a href="/notfound">Not found</a></li>
-      </ul>
-    `;
-
-    const $footer = document.createElement('div');
-    $footer.innerText = 'Footer';
-
-    const $app = document.querySelector('#app');
-    if ($app) {
-      $app.append($header, View.$mainContent, $footer);
-    }
+      document.body.append(this.$header, View.$mainContent, this.$footer);
   }
 
   static renderMainPage() {
-    View.$mainContent.innerHTML = 'Main Content';
+    View.$mainContent.innerHTML = this.$mainPage;
   }
 
   static renderRegistrationPage() {
-    View.$mainContent.innerHTML = 'Registration Content';
+    View.$mainContent.innerHTML = this.$RegistrationPage;
   }
 
   static renderLoginPage() {
-    View.$mainContent.innerHTML = 'Login Content';
+    View.$mainContent.innerHTML = this.$loginPage;
   }
 
   static render404Page() {
-    View.$mainContent.innerHTML = '404 Content';
+    View.$mainContent.innerHTML = this.$notFound;
   }
 }
