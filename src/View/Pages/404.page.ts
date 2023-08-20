35975ce62d36f1a1eb6490b9adadd6a08f24b670
Rushen Adjigeldieva <rushena2706@gmail.com>
2023-08-20 18:04:28 +0500

    refactor: use 2 space

--- src/View/Pages/404.page.ts
+++ src/View/Pages/404.page.ts
@@ -2,39 +2,39 @@ import '../../assets/css/404.css';
 import pageImage from '../../assets/img/notFoundPageImage'
 
 interface IPageNotFound {
-    pageWrap: HTMLElement,
-    createWrap: () => HTMLElement,
-    setDOMStructure: () => void,
-    getPageCode: () =>HTMLElement
+  pageWrap: HTMLElement,
+  createWrap: () => HTMLElement,
+  setDOMStructure: () => void,
+  getPageCode: () => HTMLElement
 }
 
 export class PageNotFound implements IPageNotFound {
-    private readonly pageWrap: HTMLElement;
+  private readonly pageWrap: HTMLElement;
 
-    constructor() {
-        this.pageWrap = this.createWrap();
-    }
+  constructor() {
+    this.pageWrap = this.createWrap();
+  }
 
-    private createWrap = (): HTMLElement => {
-        const $page: HTMLElement = document.createElement('div');
-        $page.classList.add('not-found');
+  private createWrap = (): HTMLElement => {
+    const $page: HTMLElement = document.createElement('div');
+    $page.classList.add('not-found');
 
-        return $page;
-    };
+    return $page;
+  };
 
-    private setDOMStructure(): void {
-        this.pageWrap.innerHTML = `
-            <div class="not-found__inner">
-                <div class="not-found__image">${pageImage()}</div>
-                <h1 class="not-found__title">Oh no. We lost this page</h1>
-                <p class="not-found__text">We searched everywhere but couldn’t find what  you’re looking for. <br />Let’s find a better place for you to go.</p>
-                <a href="/" class="not-found__button">Back to homepage</a>
-            </div>
-        `;
-    }
+  private setDOMStructure(): void {
+    this.pageWrap.innerHTML = `
+			<div class="not-found__inner">
+        <div class="not-found__image">${pageImage()}</div>
+        <h1 class="not-found__title">Oh no. We lost this page</h1>
+        <p class="not-found__text">We searched everywhere but couldn’t find what  you’re looking for. <br />Let’s find a better place for you to go.</p>
+        <a href="/" class="not-found__button">Back to homepage</a>
+			</div>
+    `;
+  }
 
-    getPageCode(): HTMLElement {
-        this.setDOMStructure();
-        return this.pageWrap;
-    }
+  getPageCode(): HTMLElement {
+    this.setDOMStructure();
+    return this.pageWrap;
+  }
 }
