10f27b34809362cff57fc0571538d1aa6ed049e1
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-08-20 15:12:13 +0300

    feat: add responsive design; fix: correct password pattern

--- src/View/pages/login.ts
+++ src/View/pages/login.ts
@@ -7,7 +7,7 @@ export default function createLoginPage(){
   document.body.insertAdjacentHTML('afterbegin',`
   <main class="login-page">
       <div class="login-page__container">
-        <img src="./src/assets/img/sign-in.jpg">
+        <img class="login-page-img" src="./src/assets/img/sign-in.jpg">
         <form class="form login-form" novalidate>
           <h2>Sign In</h2>
           <div class="form-1column-block api-error">
@@ -22,7 +22,7 @@ export default function createLoginPage(){
           <div class="form-1column-block">
             <label class="password-label">
               <sup>*</sup>Password
-              <input class="password1" type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required>
+              <input class="password1" type="password" pattern="(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}" required>
               <img src="./src/assets/img/solar_eye-broken.png">
               <span class="validation-message validation-message__password"></span>
             </label>
